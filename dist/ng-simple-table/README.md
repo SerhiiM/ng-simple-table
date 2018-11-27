484a80f32805249cfde0e6eeb6fb023838b86c6c
SerhiiM <misnik.sergei93@gmail.com>
2018-11-27 17:26:45 +0200

    v.0.0.3

--- dist/ng-simple-table/README.md
+++ dist/ng-simple-table/README.md
@@ -1,6 +1,30 @@
 # ng-simple-table
 
+For now u have to install font-awesome and bootstrap
+
+`npm install font-awesome bootstrap --save`
+
 Have to be included 
 
-  "node_modules/font-awesome/scss/font-awesome.scss",
-  "node_modules/bootstrap/dist/css/bootstrap.css",
+`
+"node_modules/font-awesome/scss/font-awesome.scss",
+"node_modules/bootstrap/dist/css/bootstrap.css"
+`
+
+In root 
+
+`import { NgSimpleTableModule } from 'ng-simple-table';`
+
+in template
+
+---
+><smt-ng-simple-table
+  [list]="[
+    {id: 1, name:'Dan'},
+    {id: 2, name:'Kate'}
+  ]"
+  [columns]="[
+    {titleKey:'Name', withSorting: true, rowKey: 'name'},
+    {titleKey:'Actions', withSorting: false, rowKey: '', withActionCol: true}]"
+></smt-ng-simple-table>
+---
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  list = [{
-    id: 1,
-    manufacturer_name: 'BMW',
-    name: 'GTX'
-  }];
+
+    columns = [
+        {titleKey: 'Manufacture Name', withSorting: true, rowKey: 'manufacturer_name'},
+        {titleKey: 'Model Name', withSorting: true, rowKey: 'name'},
+        {titleKey: 'Action', withSorting: false, rowKey: '', withActionCol: true}
+    ];
+
+    list = [
+        {
+            id: 1,
+            manufacturer_name: 'BMW',
+            name: 'GTX'
+        },
+        {
+            id: 2,
+            manufacturer_name: 'MAZDA',
+            name: 'IPD'
+        }
+    ];
+
+    meta = {
+        currentPage: 1,
+        nextPage: 1,
+        pagesCount: 1,
+        perPage: 10,
+        prevPage: 1,
+        totalCount: 5
+    };
+
+    startView(id) {
+        console.log(id);
+    }
+
+    startEditing(id) {
+        console.log(id);
+    }
+
+    sortList(sort) {
+        console.log(sort);
+    }
+
+    turnPage(page) {
+        console.log(page);
+    }
+
+    changeItemsPerPage(itemsPerPage) {
+        console.log(itemsPerPage);
+    }
+
+    handleDelete(id) {
+        console.log(id);
+    }
 }
